1691991c9d5acc6eb62ba3324eb8b3c040a74d03
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-22 00:36:20 +0300

    Add components to my home page and add the components hero and feactured-posts and its css module files to components folder in home-page folder

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
-export default function HomePage(){
-    
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
 }
+// 1) Hero => present ourselves
+// 2) Featured Posts
