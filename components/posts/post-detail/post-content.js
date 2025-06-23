65f6c794df0ac5f92bf550cb6aced64bac963538
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-23 11:39:10 +0300

    Adding the post-content and post-header components and add them to slug page with some dummy data

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,20 @@
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css"
+const DUMMY_POST = {
+  slug: "getting-started-with-nextjs",
+  title: "Getting Started with NextJS",
+  image: "getting-started-nextjs.png",
+  excerpt:
+    "NextJS is a the React framework from production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+  date: "2022-02-10",
+  content: "# This is a first post",
+};
+export default function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
