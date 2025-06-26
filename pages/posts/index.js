19879c769b6c690d3d9ee55716225e86556f3fa3
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-27 02:57:35 +0300

    Remove DUMMY-DATA from home and all posts pages and get the posts by .md files

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,38 +1,16 @@
 import AllPosts from "../../components/posts/all-posts";
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework from production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework from production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework from production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework from production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
-];
-export default function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+import { getAllPosts } from "../../lib/posts-util";
+
+export default function AllPostsPage(props) {
+  const { posts } = props;
+  return <AllPosts posts={posts} />;
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts();
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 }
