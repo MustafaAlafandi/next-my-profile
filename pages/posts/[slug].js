30db8cd5ebe3bdb4baf7944a0c8e7017b203a513
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-27 03:21:20 +0300

    Adding get post data from .md file by getSTaticProps and getStaticPaths

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,30 @@
+import { getPageFiles } from "next/dist/server/get-page-files";
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-export default function PostDetailPage() {
-  return <PostContent />;
+export default function PostDetailPage(props) {
+  const { postData } = props;
+  return <PostContent post={postData}/>;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const postData = getPostData(params.slug[0]);
+  return {
+    props: {
+      postData,
+    },
+    revalidate: 600,
+  };
+}
+export async function getStaticPaths() {
+  const paths = getPostsFiles()
+    .map((fileName) => fileName.replace(/\.md$/, ""))
+    .map((name) => ({
+      params: { slug: name },
+    }));
+  return {
+    paths,
+    fallback: "blocking",
+  };
 }
